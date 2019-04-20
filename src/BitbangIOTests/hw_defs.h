8eaccd657b14161d36152d6fe2e43e3e63829cb1
Badger <badger@centrum.cz>
2019-04-20 19:33:21 +0200

    - add bitbang tests

---
+++ src/BitbangIOTests/hw_defs.h
@@ -0,0 +1,27 @@
+#ifndef HW_DEFS_H
+#define HW_DEFS_H
+
+
+#if defined (HW_38x38_rev_0_1)
+
+#include "hw_defs_38x38_rev_0_1.h"
+
+#elif defined (HW_38x38_rev_0_3)
+
+#include "hw_defs_38x38_rev_0_3.h"
+
+#elif defined (HW_60x60_rev_0_2)
+
+#include "hw_defs_60x60_rev_0_2.h"
+
+#elif defined (HW_60x60_rev_0_4)
+
+#include "hw_defs_60x60_rev_0_4.h"
+
+#else
+
+#error HW version not defined
+
+#endif /*HW_VERSION*/
+
+#endif
---
+++ src/BitbangIOTests/miniblink.c
@@ -0,0 +1,144 @@
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
+ * Copyright (C) 2011 Stephen Caudle <scaudle@doceme.com>
+ * Copyright (C) 2012 Karl Palsson <karlp@tweak.net.au>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include "hw_defs.h"
+
+#define PORT_LED GPIOC
+#define PIN_LED GPIO15
+
+
+
+static void gpio_setup(void)
+{
+	/* Enable GPIOC clock. */
+	/* Manually: */
+	//RCC_AHBENR |= RCC_AHBENR_GPIOCEN;
+	/* Using API functions: */
+	rcc_periph_clock_enable(RCC_GPIOC);
+	rcc_periph_clock_enable(RCC_GPIOA);
+	rcc_periph_clock_enable(RCC_GPIOB);
+
+
+	/* Set GPIO8 (in GPIO port C) to 'output push-pull'. */
+	/* Using API functions: */
+	gpio_mode_setup(PORT_LED, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, PIN_LED);
+	gpio_mode_setup(DISP_nOE_ROW_GPIO  , GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,DISP_nOE_ROW_GPIO_PIN);
+	gpio_mode_setup(DISP_nOE_COL_GPIO  , GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,DISP_nOE_COL_GPIO_PIN  );
+	gpio_mode_setup(DISP_COLS_CLK_GPIO  , GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, DISP_COLS_CLK_GPIO_PIN  );
+	gpio_mode_setup(DISP_COLS_DATA_GPIO  , GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, DISP_COLS_DATA_GPIO_PIN  );
+	gpio_mode_setup(DISP_COLS_STROBE_GPIO  , GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, DISP_COLS_STROBE_GPIO_PIN  );
+}
+
+#define wait \
+		for (i = 0; i < 500; i++) {	/* Wait a bit. */\
+			__asm__("nop");\
+		}\
+
+bitbang(uint8_t col, uint8_t row)
+{
+	int i;
+	int k;
+		gpio_clear(PORT_LED, PIN_LED);	/* LED on/off */
+
+		for (k=0;k<8;k++) {
+			if(col & (1<<k)) {
+				gpio_set(DISP_COLS_DATA_GPIO, DISP_COLS_DATA_GPIO_PIN);
+			} else {
+				gpio_clear(DISP_COLS_DATA_GPIO, DISP_COLS_DATA_GPIO_PIN);
+			}
+			__asm__("nop");
+			gpio_set(DISP_COLS_CLK_GPIO, DISP_COLS_CLK_GPIO_PIN);
+			__asm__("nop");
+			gpio_clear(DISP_COLS_CLK_GPIO, DISP_COLS_CLK_GPIO_PIN);
+			__asm__("nop");
+			}
+
+		for (k=0;k<8;k++) {
+			if(row & (1<<k)) {
+				gpio_set(DISP_COLS_DATA_GPIO, DISP_COLS_DATA_GPIO_PIN);
+			} else {
+				gpio_clear(DISP_COLS_DATA_GPIO, DISP_COLS_DATA_GPIO_PIN);
+			}
+			__asm__("nop");
+			gpio_set(DISP_COLS_CLK_GPIO, DISP_COLS_CLK_GPIO_PIN);
+			__asm__("nop");
+			gpio_clear(DISP_COLS_CLK_GPIO, DISP_COLS_CLK_GPIO_PIN);
+			__asm__("nop");
+			}
+
+			/*strobe*/
+		gpio_set(DISP_COLS_STROBE_GPIO, DISP_COLS_STROBE_GPIO_PIN);
+		__asm__("nop");
+		gpio_clear(DISP_COLS_STROBE_GPIO, DISP_COLS_STROBE_GPIO_PIN);
+		__asm__("nop");
+	
+		gpio_set(PORT_LED, PIN_LED);	/* LED on/off */
+		__asm__("nop");
+
+
+}
+
+
+int main(void)
+{
+	int i;
+	int k;
+
+	gpio_setup();
+	k=1;
+
+		gpio_clear(DISP_nOE_ROW_GPIO,DISP_nOE_ROW_GPIO);
+		gpio_clear(DISP_nOE_COL_GPIO,DISP_nOE_COL_GPIO);
+	/* Blink the LED (PC8) on the board. */
+	while (1) {
+		/* Manually: */
+		// GPIOC_BSRR = PIN_LED;		/* LED off */
+		// for (i = 0; i < 1000000; i++)	/* Wait a bit. */
+		//	__asm__("nop");
+		// GPIOC_BRR = PIN_LED;			/* LED on */
+		// for (i = 0; i < 1000000; i++)	/* Wait a bit. */
+		//	__asm__("nop");
+
+		/* Using API functions gpio_set()/gpio_clear(): */
+		// gpio_set(PORT_LED, PIN_LED);		/* LED off */
+		// for (i = 0; i < 1000000; i++)	/* Wait a bit. */
+		//	__asm__("nop");
+		// gpio_clear(PORT_LED, PIN_LED);	/* LED on */
+		// for (i = 0; i < 1000000; i++)	/* Wait a bit. */
+		//	__asm__("nop");
+
+		/* Using API function gpio_toggle(): */
+		bitbang(k,0xff);
+		if(k==0x80){
+			k=1;
+			}
+		else {
+			k=k<<1;
+		}
+		 for (i = 0; i < 1000; i++)	/* Wait a bit. */
+			__asm__("nop");
+
+	}
+
+	return 0;
+}
